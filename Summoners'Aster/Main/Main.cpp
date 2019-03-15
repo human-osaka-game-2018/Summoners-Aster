e8a16a0b290611af147397dd47f5cba4d05a0813
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-15 21:37:33 +0900

    [issue #3] ウィンドウのサイズを変更

--- Summoners'Aster/Main/Main.cpp
+++ Summoners'Aster/Main/Main.cpp
@@ -6,17 +6,20 @@
 
 using WindowParam = gameframework::WindowParam;
 using Window = gameframework::Window;
+using RectSize = gameframework::RectSize;
 
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szStr, INT iCmdShow)
 {
 	WindowParam::SetInstanceHandle(hInst);
+
+	//デフォルトでは1280.0f 720.0f
+	WindowParam::SetWindowSize(RectSize(1920.0f, 1080.0f));
 
 	Window* pWindow = new Window(_T("Summoners'Aster"));
 
 	while (!pWindow->ReceivedQuitMessage())
 	{
 		if (pWindow->ReceivedWinMessage()) continue;
-
 	}
 
 	delete pWindow;
