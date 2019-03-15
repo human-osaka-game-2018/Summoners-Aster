b23991ad93262f37ff76f13ff5c8d5f23d260ec3
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-15 21:30:27 +0900

    [issue #3] WinMainを作成しウィンドウの作成を行う

---
+++ Summoners'Aster/Main/Main.cpp
@@ -0,0 +1,25 @@
+﻿#include <Windows.h>
+#include <tchar.h>
+
+#include <Window/WindowParam.h>
+#include <Window/Window.h>
+
+using WindowParam = gameframework::WindowParam;
+using Window = gameframework::Window;
+
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szStr, INT iCmdShow)
+{
+	WindowParam::SetInstanceHandle(hInst);
+
+	Window* pWindow = new Window(_T("Summoners'Aster"));
+
+	while (!pWindow->ReceivedQuitMessage())
+	{
+		if (pWindow->ReceivedWinMessage()) continue;
+
+	}
+
+	delete pWindow;
+
+	return 0;
+}
