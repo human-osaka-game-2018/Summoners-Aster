a7a6bae698e46647a796159b2ae2541b221cd651
rikita-fujizono <ricky1002fe@gmail.com>
2019-03-18 00:21:06 +0900

    [issue #5] シーン基底クラスの作成

---
+++ Summoners'Aster/SceneSwitcher/Scene/Scene.h
@@ -0,0 +1,29 @@
+﻿#ifndef SCENE_H
+#define SCENE_H
+
+class Scene
+{
+public:
+	enum class KINDS
+	{
+		TITLE,
+		HOME,
+		BATTLE,
+		RESULT
+	};
+
+	virtual ~Scene() {};
+
+	virtual void Initialize() = 0;
+
+	virtual void Finalize() = 0;
+
+	virtual void Update() = 0;
+
+	virtual void Render() = 0;
+
+protected:
+	Scene() {};
+};
+
+#endif //!SCENE_H
