24665e4b52a79243edbb74e84ed855fe8b47b8ae
rikita-fujizono <ricky1002fe@gmail.com>
2019-03-19 20:54:52 +0900

    [issue #12] デストラクタを仮想関数に変更

---
+++ Summoners-Aster/ObjectIntegrator/Object/Object.h
@@ -0,0 +1,29 @@
+﻿#ifndef OBJECT_H
+#define OBJECT_H
+
+class Object
+{
+public:
+	virtual ~Object();
+
+	virtual bool Initialize() = 0;
+	virtual bool LoadResource() = 0;
+
+	virtual void Finalize() = 0;
+
+	virtual void Update() = 0;
+	virtual void Render() = 0;
+
+	inline float GetZ() const 
+	{
+		return m_z; 
+	}
+	
+protected:
+	Object(float z);
+
+	//描画順を決める際に扱う変数
+	float m_z = 1.0f;
+};
+
+#endif //!OBJECT_H
