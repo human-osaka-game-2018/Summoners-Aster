c8f3ef043abefcaf35ba6458ae44e6961a2c9201
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-20 12:38:04 +0900

    [issue #24] ✨ オブジェクトクラスを順番に動かしたりできるクラスの追加

---
+++ Summoners-Aster/ObjectIntegrator/TaskScheduler.cpp
@@ -0,0 +1,131 @@
+﻿#include "TaskScheduler.h"
+
+namespace summonersaster
+{
+	TaskScheduler::TaskScheduler() 
+	{
+	
+	}
+
+	TaskScheduler::~TaskScheduler()
+	{
+		ReleaseAll();
+	}
+
+	void TaskScheduler::Register(Task* pTask, int frameTakesToStart, bool hasContinuedEvenIfFinished)
+	{
+		TaskData taskData;
+		taskData.m_pTask				   = pTask;
+		taskData.m_frameTakesToStart	   = frameTakesToStart;
+		taskData.m_continuedEvenIfFinished = hasContinuedEvenIfFinished;
+
+		m_tasks.push_back(taskData);
+	}
+
+	void TaskScheduler::Update()
+	{
+		ReleaseNotUsingTasks();
+		UpdateCanRunning();
+
+		for (auto& task : m_tasks)
+		{
+			if (!task.m_canRunning) continue;
+
+			(task.m_pTask)->Update();
+		}
+	}
+
+	void TaskScheduler::Render()
+	{
+		for (auto& task : m_tasks)
+		{
+			if (!task.m_canRunning) continue;
+
+			(task.m_pTask)->Render();
+		}
+	}
+
+	void TaskScheduler::ReloadResources()
+	{
+		for (auto& task : m_tasks)
+		{
+			(task.m_pTask)->LoadResource();
+		}
+	}
+
+	void TaskScheduler::UpdateCanRunning()
+	{
+		for (auto& task : m_tasks)
+		{
+			task.m_canRunning = CanRunning(task);
+		}
+	}
+
+	bool TaskScheduler::CanRunning(TaskData& task)
+	{
+		int index = static_cast<int>(&task - &m_tasks[0]);
+
+		/// <summary>
+		/// 前のタスクからの経過時間のみで動かすことができるかを判別する
+		/// </summary>
+		/// <returns>処理することができるならtrue</returns>
+		auto CanRunningInTermsOfFrameCount = [&]() -> bool
+		{
+			if ((task.m_frameTakesToStartCount++) < task.m_frameTakesToStart) return false;
+
+			task.m_frameTakesToStartCount = task.m_frameTakesToStart;
+
+			return true;
+		};
+
+		// 一番初めのタスクの場合は前のタスクがない
+		if (index == 0)
+		{
+			return CanRunningInTermsOfFrameCount();
+		}
+
+		if ((m_tasks[index - 1].m_pTask)->IsFinished())
+		{
+			return CanRunningInTermsOfFrameCount();
+		}
+
+		return false;
+	}
+
+	void TaskScheduler::ReleaseAll()
+	{
+		for (auto& task : m_tasks)
+		{
+			delete task.m_pTask;
+		}
+
+		m_tasks.clear();
+		m_tasks.shrink_to_fit();
+	}
+
+	void TaskScheduler::ReleaseNotUsingTasks()
+	{
+		for (int i = static_cast<int>(m_tasks.size() - 1); i >= 0; --i)
+		{
+			if (!IsNotUsingTask(m_tasks[i])) continue;
+
+			delete m_tasks[i].m_pTask;
+
+			// 現在のiが指し示している場所の削除
+			m_tasks.erase(m_tasks.begin() + i);
+		}
+
+		m_tasks.shrink_to_fit();
+	}
+
+	bool TaskScheduler::IsNotUsingTask(const TaskData& taskData)
+	{
+		// タスクが完了していないか
+		if (!(taskData.m_pTask->IsFinished())) return false;
+
+		// 終了していても続けるか
+		if (taskData.m_continuedEvenIfFinished) return false;
+
+		return true;
+	}
+} // namespace summonersaster
---
+++ Summoners-Aster/ObjectIntegrator/TaskScheduler.h
@@ -0,0 +1,122 @@
+﻿#ifndef TASK_SCHEDULER_H
+#define TASK_SCHEDULER_H
+
+#include <vector>
+
+#include "Object/Task.h"
+
+namespace summonersaster
+{
+	/// <summary>
+	/// Taskクラスを順番に処理するクラス
+	/// </summary>
+	class TaskScheduler
+	{
+	public:
+		TaskScheduler();
+
+		~TaskScheduler();
+
+		/// <summary>
+		/// タスクの登録を行う
+		/// </summary>
+		/// <param name="pTask">タスクのポインタ</param>
+		/// <param name="frameTakesToStart">前のタスクが終了してから動かすのにかかるフレーム数</param>
+		/// <param name="hasContinuedEvenIfFinished">タスクが終了した後も解放せずに動かすか</param>
+		void Register(Task* pTask, int frameTakesToStart, bool hasContinuedEvenIfFinished = false);
+
+		/// <summary>
+		/// 処理すべきタスクの更新を行う
+		/// </summary>
+		/// <remarks>
+		/// この関数で不必要なタスクを開放し、処理すべきタスクを調べ更新する
+		/// </remarks>
+		void Update();
+
+		/// <summary>
+		/// 処理すべきタスクの描画を行う
+		/// </summary>
+		void Render();
+
+		/// <summary>
+		/// 全てのタスクのリソースを再度読み込ませる
+		/// </summary>
+		void ReloadResources();
+
+	private:
+		/// <summary>
+		/// タスクとタスクに必要な情報をまとめた構造体
+		/// </summary>
+		struct TaskData
+		{
+		public:
+			/// <summary>
+			/// タスクのポインタ
+			/// </summary>
+			Task* m_pTask = nullptr;
+
+			/// <summary>
+			/// 前のタスクが終了してから動かすまでのフレーム数
+			/// </summary>
+			int m_frameTakesToStart = 0;
+
+			/// <summary>
+			/// タスクが終了していても動かすか
+			/// </summary>
+			bool m_continuedEvenIfFinished = false;
+
+			/// <summary>
+			/// 前のタスクが終了してから経過しているフレーム数
+			/// </summary>
+			/// <remarks>
+			/// 最大値はm_frameTakesToStartと同じ
+			/// </remarks>
+			int m_frameTakesToStartCount = 0;
+
+			/// <summary>
+			/// 動かせるかどうか
+			/// </summary>
+			/// <remarks>
+			/// TaskSchedulerのUpdateで判別する
+			/// </remarks>
+			bool m_canRunning = false;
+		};
+
+		TaskScheduler(const TaskScheduler& taskScheduler) = delete;
+
+		TaskScheduler& operator=(const TaskScheduler& taskScheduler) = delete;
+
+		/// <summary>
+		/// 全てのタスクの処理ができるかを更新する
+		/// </summary>
+		void UpdateCanRunning();
+
+		/// <summary>
+		/// 引数に渡したタスクが処理できるかを判別する
+		/// </summary>
+		/// <param name="task">処理できるかを判別したいタスク</param>
+		/// <returns>できるのならtrue</returns>
+		bool CanRunning(TaskData& task);
+
+		/// <summary>
+		/// 全てのタスクを開放する
+		/// </summary>
+		void ReleaseAll();
+
+		/// <summary>
+		/// 終了し動かす必要もないタスクを開放する
+		/// </summary>
+		void ReleaseNotUsingTasks();
+
+		/// <summary>
+		/// もう動かす必要のないタスクかどうかを判別する
+		/// </summary>
+		/// <param name="taskData">調べたいタスク</param>
+		/// <returns>もう使わないのならture</returns>
+		bool IsNotUsingTask(const TaskData& taskData);
+
+		std::vector<TaskData> m_tasks;
+	};
+}// namespace summonersaster
+
+#endif // !TASK_SCHEDULER_H
