978bbd3c299e4b283a672f55c31435ab5ab13e24
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-20 17:21:46 +0900

    [issue #7] DirectXの準備処理

--- Summoners-Aster/Main/Main.cpp
+++ Summoners-Aster/Main/Main.cpp
@@ -4,22 +4,37 @@
 #include <GameFramework.h>
 
 using namespace gameframework;
+using namespace summonersaster;
 
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szStr, INT iCmdShow)
 {
 	WindowParam::SetInstanceHandle(hInst);
 
 	//デフォルトでは1280.0f 720.0f
-	WindowParam::SetWindowSize(RectSize(1920.0f, 1080.0f));
+	WindowParam::SetWindowSize(RectSize(1600.0f, 900.0f));
 
 	Window* pWindow = new Window(_T("Summoners'Aster"));
+
+	//インスタンスが一つしかないものを自動開放するように登録
+	SingletonHolder<Window>::Create(pWindow);
+
+	//DirectXのデバイスを作成
+	DirectXParam::Create();
+
+	GameFramework& rGameFramework = GameFramework::CreateAndGetRef();
+
 
 	while (!pWindow->ReceivedQuitMessage())
 	{
 		if (pWindow->ReceivedWinMessage()) continue;
-	}
 
-	delete pWindow;
+		if (!rGameFramework.CanStartNextFrame()) continue;
+
+		rGameFramework.PrepareInFrame();
+
+
+		rGameFramework.FinishInFrame();
+	}
 
 	return 0;
 }
