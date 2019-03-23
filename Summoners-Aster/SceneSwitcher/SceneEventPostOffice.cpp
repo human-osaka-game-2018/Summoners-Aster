8061aca2906d1b150eb9066b9a6b76cdda25826e
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-23 19:49:25 +0900

    [issue #32] シーンに関するイベント伝達の仲介クラスの作成

---
+++ Summoners-Aster/SceneSwitcher/SceneEventPostOffice.cpp
@@ -0,0 +1,46 @@
+﻿#include "SceneEventPostOffice.h"
+
+namespace summonersaster
+{
+	std::vector<SceneSwitchEvent*> SceneEventPostOffice::m_pSceneSwitchEventPosts;
+
+	void SceneEventPostOffice::RegisterReceiver(SceneSwitchEvent* pSceneSwitchEventPost)
+	{
+		m_pSceneSwitchEventPosts.push_back(pSceneSwitchEventPost);
+	}
+
+	void SceneEventPostOffice::UnRegisterReceiver(SceneSwitchEvent* pSceneSwitchEventPost)
+	{
+		for (auto pPost : m_pSceneSwitchEventPosts)
+		{
+			int index = static_cast<int>(&pPost - &m_pSceneSwitchEventPosts[0]);
+
+			//ポインタが同じじゃなかったら
+			if (pPost != pSceneSwitchEventPost) continue;
+
+			//要素を削除する
+			m_pSceneSwitchEventPosts.erase(m_pSceneSwitchEventPosts.begin() + index);
+
+			return;
+		}
+	}
+
+	void SceneEventPostOffice::SendSceneSwitchEvent(Scene::KIND nextScene)
+	{
+		for (auto pPost : m_pSceneSwitchEventPosts)
+		{
+			pPost->m_nextScene = nextScene;
+			pPost->m_shouldSwitch = true;
+		}
+	}
+
+	SceneEventPostOffice::SceneEventPostOffice()
+	{
+
+	}
+
+	SceneEventPostOffice::~SceneEventPostOffice()
+	{
+
+	}
+} // namespace summonersaster
---
+++ Summoners-Aster/SceneSwitcher/SceneEventPostOffice.h
@@ -0,0 +1,60 @@
+﻿#ifndef SCENE_EVENT_POST_OFFICE_H
+#define SCENE_EVENT_POST_OFFICE_H
+
+#include <vector>
+
+#include <GameFramework.h>
+
+#include "Scene/Scene.h"
+
+namespace summonersaster
+{
+	/// <summary>
+	/// シーン切り替え情報
+	/// </summary>
+	struct SceneSwitchEvent
+	{
+		Scene::KIND m_nextScene;
+
+		/// <summary>
+		/// シーンの初期化を行うべきかを判断する
+		/// </summary>
+		/// <remarks>
+		/// 現在のシーンを初期化する必要が出てきた時に対応するため
+		/// </remarks>
+		bool m_shouldSwitch = false;
+	};
+
+	/// <summary>
+	/// シーンに関するイベント伝達の仲介クラス
+	/// </summary>
+	class SceneEventPostOffice
+	{
+	public:
+		/// <summary>
+		/// イベントの受取先の登録
+		/// </summary>
+		/// <param name="pSceneSwitchEventPost">イベントを受け取るポインタ</param>
+		static void RegisterReceiver(SceneSwitchEvent* pSceneSwitchEventPost);
+
+		/// <summary>
+		/// イベント受け取りの解除
+		/// </summary>
+		/// <param name="pSceneSwitchEventPost">イベントを受け取っているポインタ</param>
+		static void UnRegisterReceiver(SceneSwitchEvent* pSceneSwitchEventPost);
+
+		/// <summary>
+		/// 登録されている受取先全てにシーン切り替えイベントを送る
+		/// </summary>
+		/// <param name="nextScene">次からのシーン</param>
+		static void SendSceneSwitchEvent(Scene::KIND nextScene);
+
+	private:
+		SceneEventPostOffice();
+		~SceneEventPostOffice();
+
+		static std::vector<SceneSwitchEvent*> m_pSceneSwitchEventPosts;
+	};
+} // namespace summonersaster
+
+#endif //!SCENE_EVENT_POST_OFFICE_H
