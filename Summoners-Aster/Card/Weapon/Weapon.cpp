f9c2d1ffa2c08da55d086f9e682173633fcccb45
rikita-fujizono <ricky1002fe@gmail.com>
2019-04-07 00:24:17 +0900

    [issue #44] カード基底クラスの作成

---
+++ Summoners-Aster/Card/Weapon/Weapon.cpp
@@ -0,0 +1,21 @@
+﻿#include "Weapon.h"
+
+namespace summonersaster
+{
+	Weapon::Weapon(const tstring& Name, const tstring& TexturePath, int Cost, int Attack, int HitPoint) 
+		: Card(TYPE::WEAPON, Name, TexturePath, Cost), m_attack(Attack), m_hP(HitPoint)
+	{
+
+	}
+
+	Weapon::~Weapon()
+	{
+
+	}
+
+	void Weapon::CreateCopy(Card** ppCard)const
+	{
+		*ppCard = new Weapon(m_name, m_texturePath, m_cost, m_attack, m_hP);
+	}
+
+} // namespace summonersaster
---
+++ Summoners-Aster/Card/Weapon/Weapon.h
@@ -0,0 +1,30 @@
+﻿#ifndef WEAPON_H
+#define WEAPON_H
+
+#include "Card\Card.h"
+
+namespace summonersaster
+{
+	class Weapon :public Card
+	{
+	public:
+		Weapon(const tstring& Name, const tstring& TexturePath, int Cost, int Attack, int HitPoint);
+		~Weapon();
+
+		/// <summary>
+		/// 自身のコピーを動的確保する
+		/// </summary>
+		/// <param name="ppCard">コピーされるカードのポインタのポインタ</param>
+		void CreateCopy(Card** ppCard) const override;
+
+	private:
+		Weapon(Weapon& weapon) = delete;
+
+		Weapon& operator=(Weapon& weapon) = delete;
+
+		int m_hP;
+		int m_attack;
+	};
+} // namespace summonersaster
+
+#endif // !WEAPON_H
