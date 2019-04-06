1633bcdafb132bc801c906355319b79b225a7103
Harutaka-Tsujino <glaca986@outlook.jp>
2019-04-06 22:31:46 +0900

    [#52] ✨ ターンクラスの作成

---
+++ Summoners-Aster/SceneSwitcher/Scene/MainScene/Turn/Turn.h
@@ -0,0 +1,60 @@
+﻿#ifndef TURN_H
+#define TURN_H
+
+#include <unordered_map>
+
+#include <GameFramework.h>
+
+#include "TurnStage.h"
+#include "SwitcherBase.h"
+#include "TurnUIRenderingStage.h"
+#include "PhaseOperationStage.h"
+
+namespace summonersaster
+{
+	using gameframework::Singleton;
+
+	/// <summary>
+	/// ターンクラス
+	/// </summary>
+	class Turn :public Singleton<Turn>, public SwitcherBase<TurnStage>
+	{
+	public:
+		friend Singleton<Turn>;
+
+		~Turn();
+
+		/// <summary>
+		/// 現在の段階の更新処理を行う
+		/// </summary>
+		/// <remarks>
+		/// 段階を切り替える関数を呼ぶ
+		/// </remarks>
+		void Update()override;
+
+		inline const bool& GetIsPreceding()const
+		{
+			return m_isPreceding;
+		}
+
+		inline const int& GetTurnNum()const
+		{
+			return m_turnNum;
+		}
+
+	private:
+		Turn();
+
+		/// <summary>
+		/// 全てのシーンのインスタンスを生成する
+		/// </summary>
+		void CreateKindInstances()override;
+
+		void IncrementTurn();
+
+		bool m_isPreceding = false;
+		int m_turnNum = 1;
+	};
+} // namespace summonersaster
+
+#endif //!TURN_H
---
+++ Summoners-Aster/SceneSwitcher/Scene/MainScene/Turn/TurnUIRenderingStage.h
@@ -0,0 +1,60 @@
+﻿#ifndef TURN_UI_RENDERING_STAGE_H
+#define TURN_UI_RENDERING_STAGE_H
+
+#include "TurnStage.h"
+#include "SwitchEventMediatorBase.h"
+
+namespace summonersaster
+{
+	using gameframework::GameFramework;
+	using gameframework::RectSize;
+	using gameframework::Stream;
+
+	/// <summary>
+	/// 効果を発動したり攻撃したりするフェイズ
+	/// </summary>
+	class TurnUIRenderingStage :public TurnStage
+	{
+	public:
+		TurnUIRenderingStage(const bool& isPrecedingTurn, const int& turnNum);
+		~TurnUIRenderingStage();
+
+		/// <summary>
+		/// 初期化を行う
+		/// </summary>
+		void Initialize()override;
+
+		/// <summary>
+		/// 終了処理を行う
+		/// </summary>
+		void Finalize()override;
+
+		/// <summary>
+		/// 更新を行う
+		/// </summary>
+		void Update()override;
+
+		/// <summary>
+		/// 描画を行う
+		/// </summary>
+		void Render()override;
+
+	private:
+		TurnUIRenderingStage(TurnUIRenderingStage& turnUIRenderingStage) = delete;
+
+		TurnUIRenderingStage& operator=(TurnUIRenderingStage& turnUIRenderingStage) = delete;
+
+		void CreateTurnStream();
+
+		RectSize m_windowSize;
+
+		Stream* m_pStream = nullptr;
+
+		const bool& rIS_PRECEDING_TURN;
+		const int& rTURN_NUM;
+
+		GameFramework& m_rGameFramework = GameFramework::GetRef();
+	};
+} // namespace summonersaster
+
+#endif //!TURN_UI_RENDERING_STAGE_H
