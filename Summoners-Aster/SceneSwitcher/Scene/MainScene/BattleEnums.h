ab766f348b6ac93191bc572315e12a925e116b4f
Harutaka-Tsujino <glaca986@outlook.jp>
2019-04-13 15:04:40 +0900

    [#59] 対戦時に様々な場所でよく扱われる列挙体をまとめたヘッダの作成

---
+++ Summoners-Aster/SceneSwitcher/Scene/MainScene/BattleEnums.h
@@ -0,0 +1,40 @@
+﻿#ifndef BATTLE_ENUMS_H
+#define BATTLE_ENUMS_H
+
+namespace summonersaster
+{
+	/// <summary>
+	/// プレイヤーの種類
+	/// </summary>
+	enum class PLAYER_KIND
+	{
+		/// <summary>
+		/// 主体者
+		/// </summary>
+		PROPONENT,
+
+		/// <summary>
+		/// 相手
+		/// </summary>
+		OPPONENT,
+	};
+
+	/// <summary>
+	/// 戦う順序
+	/// </summary>
+	enum class BATTLE_ORDER
+	{
+		/// <summary>
+		/// 先攻
+		/// </summary>
+		FIRST,
+
+		/// <summary>
+		/// 後攻
+		/// </summary>
+		SECOND,
+	};
+
+} // namespace summonersaster
+
+#endif // ! BATTLE_ENUMS_H
