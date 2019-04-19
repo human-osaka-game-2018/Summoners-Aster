90340fb263933aec8c7939329fb09bbde9987f27
Harutaka-Tsujino <glaca986@outlook.jp>
2019-04-19 18:34:56 +0900

    [#63] 列挙体の追加

--- Summoners-Aster/SceneSwitcher/Scene/MainScene/BattleEnums.h
+++ Summoners-Aster/SceneSwitcher/Scene/MainScene/BattleEnums.h
@@ -1,12 +1,16 @@
 ﻿#ifndef BATTLE_ENUMS_H
 #define BATTLE_ENUMS_H
 
+#include <GameFramework.h>
+
 namespace summonersaster
 {
+	using gameframework::tstring;
+
 	/// <summary>
 	/// プレイヤーの種類
 	/// </summary>
-	enum class PLAYER_KIND
+	enum class PLAYER_KIND :char
 	{
 		/// <summary>
 		/// 主体者
@@ -22,7 +26,7 @@ namespace summonersaster
 	/// <summary>
 	/// 戦う順序
 	/// </summary>
-	enum class BATTLE_ORDER
+	enum class BATTLE_ORDER :char
 	{
 		/// <summary>
 		/// 先攻
@@ -35,6 +39,38 @@ namespace summonersaster
 		SECOND,
 	};
 
+	struct SCENE_KIND
+	{
+	public:
+		static const tstring TITLE;
+		static const tstring HOME;
+		static const tstring MAIN;
+		static const tstring RESULT;
+	};
+
+	struct STEP_KIND
+	{
+	public:
+		static const tstring PLAYERS_INFOMATION_RENDERING;
+		static const tstring BATTLE_PREPARING;
+		static const tstring BATTLE;
+		static const tstring RESULT;
+	};
+
+	struct TURN_STAGE_KIND
+	{
+	public:
+		static const tstring UI_RENDERING;
+		static const tstring PHASE_OPERATION;
+	};
+
+	struct PHASE_KIND
+	{
+	public:
+		static const tstring DRAW;
+		static const tstring MAIN;
+		static const tstring END;
+	};
 } // namespace summonersaster
 
 #endif // ! BATTLE_ENUMS_H
