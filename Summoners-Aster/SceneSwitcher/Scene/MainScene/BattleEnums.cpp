a3f204f9eaccd2934686069106636f567433c8c3
Harutaka-Tsujino <glaca986@outlook.jp>
2019-04-19 18:34:56 +0900

    [#63] 列挙体の追加

---
+++ Summoners-Aster/SceneSwitcher/Scene/MainScene/BattleEnums.cpp
@@ -0,0 +1,21 @@
+﻿#include "BattleEnums.h"
+
+namespace summonersaster
+{
+	const tstring STEP_KIND::PLAYERS_INFOMATION_RENDERING = _T("INFOMATION_RENDERING");
+	const tstring STEP_KIND::BATTLE_PREPARING = _T("BATTLE_PREPARING");
+	const tstring STEP_KIND::BATTLE = _T("BATTLE");
+	const tstring STEP_KIND::RESULT = _T("RESULT");
+
+	const tstring TURN_STAGE_KIND::UI_RENDERING = _T("UI_RENDERING");
+	const tstring TURN_STAGE_KIND::PHASE_OPERATION = _T("PHASE_OPERATION");
+
+	const tstring PHASE_KIND::DRAW = _T("DRAW");
+	const tstring PHASE_KIND::MAIN = _T("MAIN");
+	const tstring PHASE_KIND::END = _T("END");
+
+	const tstring SCENE_KIND::TITLE = _T("TITLE");
+	const tstring SCENE_KIND::HOME = _T("HOME");
+	const tstring SCENE_KIND::MAIN = _T("MAIN");
+	const tstring SCENE_KIND::RESULT = _T("RESULT");
+} // namespace summonersaster
